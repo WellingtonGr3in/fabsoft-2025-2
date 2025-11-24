7636295d99d7d69e4d2eec36fa45dbd7a9fd68be
Wellington Grein <wellingtongrein@univille.br>
2025-11-24 09:26:48 -0300

    Update AdminImagensController.cs

--- PitsLanches/Areas/Admin/Controllers/AdminImagensController.cs
+++ PitsLanches/Areas/Admin/Controllers/AdminImagensController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using LanchesMac.Models;
 
 
 namespace PitsLanches.Areas.Admin.Controllers
